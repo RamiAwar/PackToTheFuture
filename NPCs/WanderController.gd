7d7ec63825e9e2c19df282807b9d6f6b05eb1906
anthony2261 <antounmalkoun@gmail.com>
2020-05-02 21:19:13 +0300

    Wandering and chasing Man

--- NPCs/WanderController.gd
+++ NPCs/WanderController.gd
@@ -12,7 +12,8 @@ func _ready():
 
 func update_target_position():
 	var target_vector = Vector2(rand_range(-wander_range,wander_range), rand_range(-wander_range,wander_range))
-	target_position = start_position + target_vector
+#	target_position = start_position + target_vector
+	target_position = global_position + target_vector
 
 func get_time_left():
 	return timer.time_left
